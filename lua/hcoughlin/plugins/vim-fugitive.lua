efb592f2e3343e9e1172811ebe93c73bb8c72fa0
hcoughlin <104121493+Cthuloops@users.noreply.github.com>
2024-04-26 23:59:12 -0400

    added some more keymaps

--- lua/hcoughlin/plugins/vim-fugitive.lua
+++ lua/hcoughlin/plugins/vim-fugitive.lua
@@ -12,5 +12,12 @@ return {
     keymap.set("n", "<leader>gP", function()
             vim.cmd.Git({'pull', '--rebase'})
         end, {desc = "Pull and rebase"})
+    keymap.set("n", "<leader>ga", function()
+            vim.cmd.Git('add %')
+        end, {desc = "Add current file to commit"})
+    keymap.set("n", "<leader>gc", function()
+            vim.cmd.Git('commit')
+        end, {desc = "Commit"})
+
     end
 }
