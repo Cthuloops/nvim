780df2bd6974a7b71a1dbcab26163aa98dacf495
hcoughlin <104121493+Cthuloops@users.noreply.github.com>
2024-04-26 23:14:12 -0400

    Added some keymaps thanks to the Primeagen

--- lua/hcoughlin/plugins/vim-fugitive.lua
+++ lua/hcoughlin/plugins/vim-fugitive.lua
@@ -1,3 +1,16 @@
 return {
-    "tpope/vim-fugitive"
+    "tpope/vim-fugitive",
+
+    config = function()
+
+    local keymap = vim.keymap
+
+    keymap.set("n", "<leader>gs", vim.cmd.Git, {desc = "git status, thanks TPope/Primeagen"})
+    keymap.set("n", "<leader>gp", function()
+            vim.cmd.Git('push')
+        end, {desc = "Push this file to the repo?"})
+    keymap.set("n", "<leader>gP", function()
+            vim.cmd.Git({'pull', '--rebase'})
+        end, {desc = "Pull and rebase"})
+    end
 }
